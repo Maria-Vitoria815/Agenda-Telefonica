aca2d2fa435a2ff081d67438ea99822bfaca8aff
Maria-Vitoria815 <vitoriamellomv028@outlook.com>
2024-09-09 15:31:48 -0300

    add

--- Atividade/src/basicas/Contato.java
+++ Atividade/src/basicas/Contato.java
@@ -1,5 +1,45 @@
 package basicas;
 
 public class Contato {
+	
+	private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Contato(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+  
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Endereço: " + endereco + ", Telefone: " + telefone;
+    }
 
 }
